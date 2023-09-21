9e31b61f433e5bc452cfab0f48c4947f46f6643a
jefersonknop <jefersonknop@gmail.com>
2023-09-20 21:19:51 -0300

    primeiro commit

---
+++ src/main/java/urbanview/UrbanviewApiApplication.java
@@ -0,0 +1,13 @@
+package urbanview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanviewApiApplication.class, args);
+	}
+
+}
